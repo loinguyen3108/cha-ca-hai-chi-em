e5ff3e20665376d3b7ea8ff4b8788f7e232aec9e
loi <loi.nguyen@xomad.com>
2024-11-16 13:37:04 +0700

    Refactor logic to create importer and order

--- src/modules/routes.py
+++ src/modules/routes.py
@@ -78,53 +78,12 @@ def products():
 @login_required
 def product_import():
     if request.method == 'POST':
-        from src.models.importer import Importer
         from src.services.importer import ImporterService
-        from src.services.import_line import ImportLineService
-        from src.services.product import ProductService
+
         importer_service = ImporterService()
-        import_line_service = ImportLineService()
-        product_service = ProductService()
-
-        importer = importer_service.create_importer({
-            'imported_at': request.form['import_date'], 'imported_by': current_user.id,
-            'other_expenses': request.form['other_expenses']
-        })
-
-        import_lines_mapped = {}
-        for k, v in request.form.items():
-            if k.startswith('product_'):
-                product_id = int(k.split('_')[-1])
-                import_lines_mapped[product_id] = import_lines_mapped.get(product_id, {})
-                if 'quantity' in k:
-                    import_lines_mapped[product_id]['quantity'] = int(v)
-                if 'unit_price' in k:
-                    import_lines_mapped[product_id]['unit_price'] = float(v)
-
         try:
-            import_lines_dict = []
-            for product_id, import_line in import_lines_mapped.items():
-                if import_line['quantity'] is None or import_line['unit_price'] is None:
-                    raise ValueError('Quantity and unit price are required')
-                if import_line['unit_price'] <= 0:
-                    raise ValueError('Unit price must be greater than 0')
-                if import_line['quantity'] < 0:
-                    raise ValueError('Quantity must be greater than or equal to 0')
-
-                if import_line['quantity'] == 0:
-                    continue
-
-                import_lines_dict.append({
-                    'importer_id': importer.id, 'product_id': product_id,
-                    'quantity': import_line['quantity'], 'unit_price': import_line['unit_price']
-                })
-            if not import_lines_dict:
-                raise ValueError('No any records to import! Please check your input data.')
-
-            import_lines = import_line_service.create_import_lines(import_lines_dict) or []
-            importer.status = Importer.STATUS_SUCCESS
-            importer_service.update_importer(importer)
-            product_service.update_stock_quantity_from_import_lines(import_lines)
+            _, import_lines = importer_service.create_importer_from_request_form_form(
+                request, current_user)
             alert_message = f'Imported {len(import_lines)} records'
             flash(alert_message, 'success')
             logger.info(alert_message)
@@ -156,58 +115,11 @@ def customers():
 @login_required
 def order_tracking():
     if request.method == 'POST':
-        from src.models.order import Order
-        from src.services.customer import CustomerService
         from src.services.order import OrderService
-        from src.services.order_line import OrderLineService
-        from src.services.product import ProductService
 
-        customer_service = CustomerService()
         order_service = OrderService()
-        order_line_service = OrderLineService()
-        product_service = ProductService()
-
-        customer = customer_service.create_customer(**{'name': request.form['customer']})
-
-        order = order_service.create_order({
-            'order_at': request.form['ordered_date'], 'created_by': current_user.id,
-            'customer_id': customer.id
-        })
-
-        order_lines_mapped = {}
-        for k, v in request.form.items():
-            if k.startswith('product_'):
-                product_id = int(k.split('_')[-1])
-                order_lines_mapped[product_id] = order_lines_mapped.get(product_id, {})
-                if 'quantity' in k:
-                    order_lines_mapped[product_id]['quantity'] = int(v)
-                if 'sale_price' in k:
-                    order_lines_mapped[product_id]['sale_price'] = float(v)
-
         try:
-            order_lines_dict = []
-            for product_id, order_line in order_lines_mapped.items():
-                if order_line['quantity'] is None or order_line['sale_price'] is None:
-                    raise ValueError('Quantity and sale price are required')
-                if order_line['sale_price'] <= 0:
-                    raise ValueError('Sale price must be greater than 0')
-                if order_line['quantity'] < 0:
-                    raise ValueError('Quantity must be greater than or equal to 0')
-
-                if order_line['quantity'] == 0:
-                    continue
-
-                order_lines_dict.append({
-                    'order_id': order.id, 'product_id': product_id,
-                    'quantity': order_line['quantity'], 'sale_price': order_line['sale_price']
-                })
-            if not order_lines_dict:
-                raise ValueError('No any records to import! Please check your input data.')
-
-            order_lines = order_line_service.create_order_lines(order_lines_dict) or []
-            order.status = Order.STATUS_SUCCESS
-            order_service.update_order(order)
-            product_service.update_stock_quantity_from_order_lines(order_lines)
+            _, order_lines = order_service.create_order_from_request_form(request, current_user)
             alert_message = f'Imported {len(order_lines)} records'
             flash(alert_message, 'success')
             logger.info(alert_message)
--- src/services/importer.py
+++ src/services/importer.py
@@ -1,3 +1,5 @@
+from functools import cached_property
+
 from src.models import Importer
 from src.services import BaseService
 
@@ -6,6 +8,16 @@ class ImporterService(BaseService):
     def __init__(self):
         super().__init__()
 
+    @cached_property
+    def import_line_service(self):
+        from src.services.import_line import ImportLineService
+        return ImportLineService()
+
+    @cached_property
+    def product_service(self):
+        from src.services.product import ProductService
+        return ProductService()
+
     def create_importer(self, importer_dict: dict) -> Importer:
         if not importer_dict:
             raise ValueError('importer_dict is required')
@@ -13,5 +25,51 @@ class ImporterService(BaseService):
         importer = Importer(**importer_dict)
         return self.repo.upsert(importer)
 
+    def create_importer_from_request_form_form(self, request_form, current_user):
+
+        importer = self.create_importer({
+            'imported_at': request_form.form['import_date'], 'imported_by': current_user.id,
+            'other_expenses': request_form.form['other_expenses']
+        })
+
+        import_lines_mapped = {}
+        for k, v in request_form.form.items():
+            if k.startswith('product_'):
+                product_id = int(k.split('_')[-1])
+                import_lines_mapped[product_id] = import_lines_mapped.get(product_id, {})
+                if 'quantity' in k:
+                    import_lines_mapped[product_id]['quantity'] = int(v)
+                if 'unit_price' in k:
+                    import_lines_mapped[product_id]['unit_price'] = float(v)
+
+        import_lines = self._process_import_lines(import_lines_mapped, importer)
+        importer.status = Importer.STATUS_SUCCESS
+        self.update_importer(importer)
+        return importer, import_lines
+
     def update_importer(self, importer: Importer) -> Importer:
         return self.repo.upsert(importer)
+
+    def _process_import_lines(self, import_lines_mapped: dict, importer: Importer):
+        import_lines_dict = []
+        for product_id, import_line in import_lines_mapped.items():
+            if import_line['quantity'] is None or import_line['unit_price'] is None:
+                raise ValueError('Quantity and unit price are required')
+            if import_line['unit_price'] <= 0:
+                raise ValueError('Unit price must be greater than 0')
+            if import_line['quantity'] < 0:
+                raise ValueError('Quantity must be greater than or equal to 0')
+
+            if import_line['quantity'] == 0:
+                continue
+
+            import_lines_dict.append({
+                'importer_id': importer.id, 'product_id': product_id,
+                'quantity': import_line['quantity'], 'unit_price': import_line['unit_price']
+            })
+        if not import_lines_dict:
+            raise ValueError('No any records to import! Please check your input data.')
+
+        import_lines = self.import_line_service.create_import_lines(import_lines_dict) or []
+        self.product_service.update_stock_quantity_from_import_lines(import_lines)
+        return import_lines
